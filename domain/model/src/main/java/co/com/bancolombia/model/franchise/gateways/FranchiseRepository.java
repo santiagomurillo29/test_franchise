ebbe72f412c6e21648e41f88f20c0574cefadcd3
santiagomurillo29 <smurillocano29@gmail.com>
2025-12-26 16:04:43 -0500

    initialize project with clean architecture scaffold

---
+++ domain/model/src/main/java/co/com/bancolombia/model/franchise/gateways/FranchiseRepository.java
@@ -0,0 +1,4 @@
+package co.com.bancolombia.model.franchise.gateways;
+
+public interface FranchiseRepository {
+}
---
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/franchise/FranchiseUseCase.java
@@ -0,0 +1,6 @@
+package co.com.bancolombia.usecase.franchise;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class FranchiseUseCase {
+}
